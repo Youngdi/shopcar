4ef211877602aaeb285bf1f091db308fb25ebfb1
a92cu <a92cu@yahoo.com.tw>
2022-09-15 18:40:56 +0800

    update

--- shopcar.js
+++ shopcar.js
@@ -121,7 +121,7 @@ function addToCar(e, id, price) {
       total: totalPrice,
     },
   };
-  
+
   const check_shopcar = document.getElementById("check_shopcar");
   let shopcar_detail_header = `<h1><i class="fa-solid fa-cart-shopping" style="padding: 5px;"></i>購物車</h1>`;
   let shopcar_detail = "";
@@ -133,12 +133,32 @@ function addToCar(e, id, price) {
       `
       <div class="shopcar_row">
         <a style="color:black; font-size: 20px">${ITEM.find((i) => i.id === key).name}</a>
-        <span style="color: black; font-size: 20px" class="price">$${value.total} 
-        </span>
+        
+      <span style="color: black; font-size: 20px" class="price">$${value.total} 
+        <div><ul class="counter">
+        <li id="plus"><input type="button" onclick="adder()" value="+" /></li>
+        <li id="countCar">0</li>
+        <li id="minus"><input type="button" onclick="minuser()" value="-" /></li>
+        </ul></div><br></span>
       </div>`;
 
     bill = bill + value.total;
   }
+  function adder() {
+    var count = document.getElementById("countCar").innerHTML;
+    count = parseInt(count) + 1;
+    document.getElementById("countCar").innerHTML = count;
+  }
+  function minuser() {
+    var count = document.getElementById("countCar").innerHTML;
+    if (count <= 0) {
+      count = 0;
+    } else {
+      count = parseInt(count) - 1;
+    }
+    document.getElementById("countCar").innerHTML = count;
+  }
+
 
   let shopcar_detail_footer = `<hr />
     <div class="shopcar_footer">
